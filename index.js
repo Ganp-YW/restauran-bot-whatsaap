99dacba9d7bf02b824169981809f088a5de77231
Ganp-YW <gerardowateyma@gmail.com>
2026-04-02 14:11:57 -0400

    Optimize RAM usage and Chromium flags for Render Free tier

--- index.js
+++ index.js
@@ -131,7 +131,16 @@ async function main() {
             backupSyncIntervalMs: 300000
         }),
         puppeteer: {
-            args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
+            args: [
+                '--no-sandbox', 
+                '--disable-setuid-sandbox', 
+                '--disable-dev-shm-usage',
+                '--disable-accelerated-2d-canvas',
+                '--no-first-run',
+                '--no-zygote',
+                '--single-process',
+                '--disable-gpu'
+            ],
             headless: true
         }
     });
